d46aa2cc5016588bf6eddf311c8a92f1da3f9420
weber lucas <weber13lucas@gmail.com>
2025-08-21 22:40:09 -0300

    Adiciona mais testes

--- api-rest-app/src/controllers/transferController.js
+++ api-rest-app/src/controllers/transferController.js
@@ -3,12 +3,16 @@ const router = express.Router();
 import transferService from '../services/transferService.js';
 
 router.post('/', (req, res) => {
-    const { sender, recipient, amount } = req.body;
-    const transferResult = transferService.processTransfer(sender, recipient, amount);
-    if (transferResult.error) {
-        return res.status(400).json({ error: transferResult.error });
+    try {
+        const { sender, recipient, amount } = req.body;
+        const transferResult = transferService.processTransfer(sender, recipient, amount);
+        if (transferResult.error) {
+            return res.status(400).json({ error: transferResult.error });
+        }
+        return res.status(200).json({ message: 'Transfer successful', transfer: transferResult });
+    } catch (err) {
+        return res.status(400).json({ error: err.message });
     }
-    return res.status(200).json({ message: 'Transfer successful', transfer: transferResult });
 });
 
 router.get('/history/:userId', (req, res) => {
--- api-rest-app/test/controller/transferController.test.js
+++ api-rest-app/test/controller/transferController.test.js
@@ -3,6 +3,8 @@ import sinon from 'sinon';
 import * as chai from 'chai';
 const { expect } = chai;
 import app from '../../src/app.js';
+import transferService from '../../src/services/transferService.js';
+
 
 describe('Transfer Controller', () => {
     describe('Post/transfers', () => {
@@ -14,9 +16,34 @@ describe('Transfer Controller', () => {
             });
             expect(resposta.status).to.equal(400);
         });
+        it('usando mocha', async () => {
+            const transferServiceMock = sinon.stub(transferService, 'processTransfer')
+            transferServiceMock.throws(new Error('Invalid amount'));
+            const resposta = await request(app).post('/transfers').send({
+                sender: "",
+                recipient: "",
+                amount: 100
+            });
+            expect(resposta.status).to.equal(400);
+            expect(resposta.body).to.have.property('error', 'Invalid amount');
+            sinon.restore();
+        });
+         it('usando mocha com valores validos', async () => {
+            const transferServiceMock = sinon.stub(transferService, 'processTransfer')
+            transferServiceMock.returns({ sender: "1", recipient: "2", amount: 100 });
+            const resposta = await request(app).post('/transfers').send({
+                sender: "1",
+                recipient: "2",
+                amount: 100
+            });
+            expect(resposta.status).to.equal(200);
+            expect(resposta.body).to.have.property('message', 'Transfer successful');
+            expect(resposta.body).to.have.property('transfer');
+            sinon.restore();
+        });
     });
 
     describe('GET /transfers', () => {
         // Adicione seus testes aqui
     });
-});
+});       
